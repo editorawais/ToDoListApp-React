62b9f855aec16d44cc13e9841f6ce7329c2f794e
editorawais <editorawais@gmail.com>
2020-10-07 15:06:01 +0200

    Updates

---
+++ precache-manifest.2c48e17e5afeb5b9c46f968c6275800a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d1257a4148cd56e177fb1e83125643",
+    "url": "/ToDoListApp-React/index.html"
+  },
+  {
+    "revision": "94e15e162be89fa98513",
+    "url": "/ToDoListApp-React/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e6d4c4eaec97390d9d58",
+    "url": "/ToDoListApp-React/static/css/main.e26e44da.chunk.css"
+  },
+  {
+    "revision": "94e15e162be89fa98513",
+    "url": "/ToDoListApp-React/static/js/2.34a91291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoListApp-React/static/js/2.34a91291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d4c4eaec97390d9d58",
+    "url": "/ToDoListApp-React/static/js/main.c30f8ce3.chunk.js"
+  },
+  {
+    "revision": "2bb8e6ae5af391cbfc12",
+    "url": "/ToDoListApp-React/static/js/runtime-main.adc3f4ad.js"
+  }
+]);
